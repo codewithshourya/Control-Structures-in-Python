6f2076c65730fbcf844dc24f89b23b35746d06a3
codewithshourya <112952083+codewithshourya@users.noreply.github.com>
2025-02-25 16:26:45 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,7 @@
+First_Number = int(input("Enter the first number: "))
+Second_Number = int(input("Enter the second number: "))
+
+print("Addition: ", First_Number + Second_Number)
+print("Subtraction: ", First_Number - Second_Number)
+print("Multiplication: ", First_Number * Second_Number)
+print("Division: ", First_Number / Second_Number)
